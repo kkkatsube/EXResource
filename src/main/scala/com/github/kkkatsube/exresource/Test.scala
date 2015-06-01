0e5027b6d9180298ee3fd51cdefe55edccaed5a2
kkkatsube <k.katsube@live.jp>
2015-06-01 18:26:52 +0900

    add Test

---
+++ src/main/scala/com/github/kkkatsube/exresource/Test.scala
@@ -0,0 +1,12 @@
+package com.github.kkkatsube.exresource
+
+class Test
+{
+  def helloWorld(flag:Boolean):String =
+  {
+    if( flag )
+      "Hello World!"
+    else
+      "Hello World?"
+  }
+}
---
+++ src/test/scala/com/github/kkkatsube/exresource/TestSpec.scala
@@ -0,0 +1,21 @@
+package com.github.kkkatsube.exresource
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{WordSpec, MustMatchers}
+
+@RunWith( classOf[JUnitRunner] )
+class TestSpec extends WordSpec with MustMatchers {
+
+  object Test extends Test
+
+  "Test" must {
+    "say hello true" in {
+      Test.helloWorld( true ) must equal ("Hello World!")
+    }
+    "say hello false" in {
+      Test.helloWorld( false ) must equal ("Hello World?")
+    }
+  }
+
+}
